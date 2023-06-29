b2841e4ecb249a16ea72f238676999902ffa5077
laiba ahsan <laiba.ahsan27@gmail.com>
2023-06-29 17:05:00 +0530

    ceated

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": 'd856850e83mshf87f1efc640bf4dp196cd4jsne4d64ef3dafc',// enter your rapid api key here
+      "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+    },
+  };
+  export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+  
+  export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+  export const WEATHER_API_KEY = "bb34ca3319eae364de150b36ff0e2503"; // enter your key from openweather API
+  
